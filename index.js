339a20612eec59186e76bd5bdc1fffb494801d73
ivan olowo <ivanolowo@ivans-MacBook-Pro.local>
2021-04-02 20:47:08 +0200

    added worthing with query strings

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ app.get('/cats', (req, res) => {
     res.send('MEOW!')
 })
 
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
+})
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    res.send(`<h1>Search results for: ${q}</h1>`)
+})
+
+app.get('*', (req, res) => {
+    res.send(`I do not know that path!`)
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
 })
